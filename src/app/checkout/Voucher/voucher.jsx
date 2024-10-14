481e2b3d134d73898a482e07101eccd68077f26d
MurtadhaMohammed <mumu9943@gmail.com>
2024-10-14 15:20:20 +0300

    update2

--- src/app/checkout/Voucher/voucher.jsx
+++ src/app/checkout/Voucher/voucher.jsx
@@ -8,13 +8,16 @@ const Voucher = () => {
         <HiOutlineTicket className="text-[18px]" />
         <p className="mr-[6px]">كود الخصم</p>
       </div>
-      <div className="p-[16px] pt-0">
+      <div className="p-[16px] pt-0 relative">
         <div className="flex gap-2">
           <input
-            className="flex-1 rounded-[8px] border border-[#eee] h-[48px] pl-[16px] pr-[16px] bg-[#F6F6F6] text-[16px] outline-none"
+            className="rounded-[8px] border border-[#eee] h-[48px] pl-[16px] pr-[16px] bg-[#F6F6F6] text-[16px] outline-none"
             placeholder="ادخل رمز التخفيظ هنا"
+            style={{
+                width: "calc(100% - 80px)"
+            }}
           />
-          <Button className="bg-gradient-to-r from-indigo-600 to-violet-600 text-[#fff]">
+          <Button className="bg-gradient-to-r from-indigo-600 to-violet-600 text-[#fff] w-[80px] flex items-center justify-center">
             <span className="text-[16px] -mt-[1px] ml-[12px] mr-[12px]">
               تطبيق
             </span>
