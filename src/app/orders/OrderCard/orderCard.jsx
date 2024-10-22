8930393df07134e16a7aee8ad9a24d82c191b0bb
Baker Alazzawi <baker.alazzawi0@gmail.com>
2024-10-22 16:09:04 +0300

    order history doesnt work

---
+++ src/app/orders/OrderCard/orderCard.jsx
@@ -0,0 +1,125 @@
+import { IoMdTime } from "react-icons/io";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import styles from "./style.module.css";
+
+const ImageGroup = ({ images }) => {
+  return (
+    <div className="w-[100px] h-[100px]  relative ">
+      {images?.length === 1 && (
+        <div className="bg-[#f6f6f6] border border-[#eee] w-[100%] h-[100%] rounded-[8px]"></div>
+      )}
+
+      {images?.length === 2 && (
+        <div className="relative h-full border border-[#eee] rounded-[8px] overflow-hidden">
+          <div className="bg-[#f6f6f6] border border-[#eee] w-[100%] h-[100%] rounded-[8px]"></div>
+          <div className=" absolute inset-0 bg-[#0000002e] flex items-center justify-center text-[#fff]">
+            +1
+          </div>
+        </div>
+      )}
+
+      {images?.length === 3 && (
+        <div
+          className="relative h-full grid grid-cols-2 gap-1  overflow-hidden"
+          style={{ direction: "ltr" }}
+        >
+          <div className="bg-[#f6f6f6] border border-[#eee] w-[100%] h-[100%] rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] w-[100%] h-[100%] rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] w-[100%] h-[100%] rounded-[8px]"></div>
+        </div>
+      )}
+      {images?.length === 4 && (
+        <div
+          className="relative h-full grid grid-cols-2 gap-1  overflow-hidden"
+          style={{ direction: "ltr" }}
+        >
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+        </div>
+      )}
+      {images?.length > 4 && (
+        <div
+          className="relative h-[100%] grid grid-cols-2 gap-1  overflow-hidden"
+          style={{ direction: "ltr" }}
+        >
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+          <div className="relative h-full rounded-[8px] overflow-hidden">
+            <div className="bg-[#f6f6f6] border border-[#eee] aspect-1 rounded-[8px]"></div>
+            <div className=" absolute inset-0 bg-[#0000002e] flex items-center justify-center text-[#fff]">
+              +2
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const OrderCard = ({ order }) => {
+  const { order_status, images } = order.order || {};
+console.log(order, "order");
+  const statusTheme = {
+    Pending: {
+      color: "text-[#faad14]",
+      text: "طلبك قيد الموافقة",
+      bar: `bg-gradient-to-r from-[#faad14] to-[#faca69] ${styles.statusBar30}`,
+    },
+    Canceled: {
+      color: "text-[#ff4d4f]",
+      bar: "bg-gradient-to-r from-[#ff4d4f] to-[#fb797b]",
+      text: "تم الغاء طلبك",
+    },
+    Processing: {
+      color: "text-[#52c41a]",
+      border: "border-[#52c41a]",
+      text: "طلبك قيد التجهيز",
+      bar: `bg-gradient-to-r from-[#52c41a] to-[#8dee5e] ${styles.statusBar60}`,
+    },
+    Completed: {
+      color: "text-[#000]",
+      bg: "bg-gradient-to-r from-[#52c41a] to-[#8dee5e]",
+      text: "تم التسليم منذ 4 يوم",
+    },
+  };
+  const formattedDate = new Date(order.created_at).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+  return (
+    <div
+      className={`border border-[#eee] rounded-[16px] overflow-hidden mt-[8px]  mb-[18px]`}
+      style={{    boxShadow: "0px 5px 20px -10px #0000002b"}}
+    >
+      <div className="flex p-[16px]">
+        <ImageGroup images={images} />
+        <div className="mr-[12px] flex flex-col justify-evenly flex-1">
+          <b className={`text-[18px] ${statusTheme[order_status]?.color}`}>
+            {statusTheme[order_status]?.text}
+          </b>
+          <div className="flex items-center text-[14px] text-[#666]">
+            <IoMdTime />
+            <p className="mr-[4px]">{formattedDate}</p>
+          </div>
+          <div className="flex items-center text-[14px] text-[#666]">
+            <HiOutlineLocationMarker />
+            <p className="mr-[4px]">بغداد - شارع المريخ</p>
+          </div>
+          <div className="h-[4px] rounded-[24px] bg-[#eee]">
+            <div
+              className={`h-[4px] rounded-[24px] ${statusTheme[order_status]?.bar}`}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
---
+++ src/app/orders/OrderList/orderList.jsx
@@ -0,0 +1,76 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import Container from "@/components/UI/Container/container";
+import OrderCard from "../OrderCard/orderCard";
+import { useAppStore } from "@/lib/store";
+import { apiCall } from "@/lib/api";
+
+const OrderList = ({ params }) => {
+  const { getUserInfo } = useAppStore();
+  const user = getUserInfo();
+
+
+  const {
+    data: orders,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: [`orders-${user.id}`],
+    queryFn: () =>
+      apiCall({
+        pathname: `/client/order/getOrdersByUserId/${user.id}`,
+        method: "GET",
+        cache: "no-store",
+      }),
+  });
+
+  console.log("Orders Data:", orders);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Failed to load orders.</div>;
+  }
+
+  return (
+    <div className="mt-[16px]">
+      <Container>
+        {orders?.orders?.filter(
+          (el) =>
+            el.order_status !== "Completed" && el.order_status !== "Canceled"
+        )?.length !== 0 && (
+          <div className="flex gap-4 items-center mb-[16px] text-[16px]">
+            <span className="block h-[1px] flex-1 bg-[#f0f0f0]" />
+            <div className="text-[#666]">الطلبات النشطة</div>
+            <span className="block h-[1px] flex-1 bg-[#f0f0f0]" />
+          </div>
+        )}
+        {orders?.orders
+          ?.filter(
+            (el) =>
+              el.order_status !== "Completed" && el.order_status !== "Canceled"
+          )
+          ?.map((el, i) => (
+            <OrderCard key={i} order={el} />
+          ))}
+        <div className="flex gap-4 items-center mb-[16px] text-[16px]">
+          <span className="block h-[1px] flex-1 bg-[#f0f0f0]" />
+          <div className="text-[#666]">الطلبات السابقة</div>
+          <span className="block h-[1px] flex-1 bg-[#f0f0f0]" />
+        </div>
+        {orders?.orders
+          ?.filter(
+            (el) =>
+              el.order_status === "Completed" || el.order_status === "Canceled"
+          )
+          ?.map((el, i) => (
+            <OrderCard key={i} order={el} />
+          ))}
+      </Container>
+    </div>
+  );
+};
+
+export default OrderList;
