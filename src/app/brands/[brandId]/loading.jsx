04801b81b19f8295389f769227a9514322540ebf
MurtadhaMohammed <mumu9943@gmail.com>
2024-10-23 15:59:46 +0300

    update

---
+++ src/app/brands/[brandId]/loading.jsx
@@ -0,0 +1,12 @@
+"use client";
+import ProductSkeleton from "./Skeleton/skeleton";
+
+const Loading = () => {
+  return (
+    <div className="pb-[100px]">
+      <ProductSkeleton />
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/brands/page.js
+++ src/app/brands/page.js
@@ -7,11 +7,11 @@ export default async function Brands() {
     method: "GET",
     cache: "no-cache",
   });
-  
+
   if (!res.ok) throw new Error("Failed to fetch data");
-  
+
   const data = await res.json();
-  
+
   return (
     <div className="pb-[100px]">
       <BrandList brands={data.records} />
