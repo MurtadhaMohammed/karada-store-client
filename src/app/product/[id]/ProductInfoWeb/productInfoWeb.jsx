0534d8e0cada737f4657b9d82807e99b707658a4
MurtadhaMohammed <mumu9943@gmail.com>
2024-10-29 00:12:32 +0300

    update

--- src/app/product/[id]/ProductInfoWeb/productInfoWeb.jsx
+++ src/app/product/[id]/ProductInfoWeb/productInfoWeb.jsx
@@ -34,7 +34,7 @@ const OptionTag = ({ name, active = false, onClick }) => {
 };
 
 const ProductInfoWeb = ({ product }) => {
-  const [setIsFavorite] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [activeOption, setActiveOption] = useState(null);
   const { addItem } = useCartStore();
