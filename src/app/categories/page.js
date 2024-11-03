d1480744b3cbdf87b1a63a455691436b90679105
MurtadhaMohammed <mumu9943@gmail.com>
2024-11-03 15:57:59 +0300

    update

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -4,7 +4,9 @@ import Categories from "@/components/Categories/categories";
 import { URL } from "@/lib/api";
 
 async function getCategories() {
-  const res = await fetch(`${URL}/client/category/category`);
+  const res = await fetch(`${URL}/client/category/category?limit=100`, {
+    cache: "no-cache",
+  });
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.json();
 }
