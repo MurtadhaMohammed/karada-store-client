03b8bf67f6feb0c39935d48c51bcf1eef6b3a898
Kadhim Salah Kadhim <ka02zm45@gmail.com>
2024-11-13 12:06:52 +0300

    metadata product

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,7 +1,19 @@
 import ProductInfo from "./ProductInfo/productInfo";
 import ProductInfoWeb from "./ProductInfoWeb/productInfoWeb";
 import RelatedList from "./RelatedList/relatedList";
-import { URL } from "@/lib/api";
+import { URL, IMAGE_URL } from "@/lib/api"; 
+
+export async function generateMetadata({ params }) {
+  const id = params
+
+  const product = await fetch(`${URL}/client/product/product/${id}`).then((res) => res.json());
+  return {
+    title: product?.product?.name,
+    description: product?.product?.shortDescription,
+    images: `${IMAGE_URL}/${product?.product?.image[0].url}`,
+  }
+}
+
 
 export default async function ProductOne({ params }) {
   let products = await fetch(`${URL}/client/product/product/${params.id}`, {
@@ -9,7 +21,6 @@ export default async function ProductOne({ params }) {
     cache: "no-cache",
   });
   let product = await products.json();
-
   return (
     <div className="pb-[100px]">
       <ProductInfo product={product?.product} />
