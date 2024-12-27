cfca50a61bb08ac2da15f35a1c5ef16b27456091
Fatima Alaa <91639370+pulloxz@users.noreply.github.com>
2024-12-28 00:20:03 +0300

    add dynamic home page

---
+++ src/app/view/[id]/page.js
@@ -0,0 +1,85 @@
+import SearchBar from "@/components/SearchBar/searchBar";
+import Categories from "@/components/Categories/categories";
+import SliderBanner from "@/components/SliderBanner/sliderBanner";
+import ListBanner from "@/components/ListBanner/listBanner";
+import SingleBanner from "@/components/SingleBanner/singleBanner";
+import OffersBanner from "@/components/offersBanner/offersBanner";
+import SingleBannerPure from "@/components/SingleBannerPure/singleBannerPure";
+
+import { URL } from "@/lib/api";
+
+async function getViews(id) {
+  const res = await fetch(`${URL}/client/view/viewById/${id}`, {
+    method: "GET",
+    cache: "no-cache",
+  });
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return res.json();
+}
+
+export default async function DynamicHomePage({ params }) {
+  const { id } = params; 
+
+  try {
+    const viewData = await getViews(id);
+
+    const banners = viewData.banners;
+
+    const renderBanner = (banner) => {
+      switch (banner.type) {
+        case "Slider":
+          return <SliderBanner key={banner.id} banners={banner} />;
+        case "SinglePure":
+          return <SingleBannerPure key={banner.id} banner={banner} />;
+        case "Single":
+          return <SingleBanner key={banner.id} banner={banner} />;
+        case "List":
+          return (
+            <ListBanner
+              bannerId={banner.id}
+              key={banner.id}
+              title={banner.title}
+              list={banner?.products || []}
+            />
+          );
+        case "OfferBanner":
+          return (
+            <OffersBanner
+              bannerId={banner.id}
+              key={banner.id}
+              title={banner.title}
+              list={banner?.products || []}
+            />
+          );
+        case "Category":
+          return <Categories key={banner.id} list={banner.categories} />;
+        case "CreativeBanner":
+          return (
+            <ListBanner
+              bannerId={banner.id}
+              key={banner.id}
+              title={banner.title}
+              list={banner?.products || []}
+              isCreative
+            />
+          );
+        default:
+          return null;
+      }
+    };
+
+    return (
+      <div className="pb-[100px]">
+        <SearchBar />
+        {banners && banners.length > 0 ? (
+          banners.map((banner) => renderBanner(banner))
+        ) : (
+          <div>No banners available</div>
+        )}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching view data:", error);
+    return <div>Failed to load content</div>;
+  }
+}
