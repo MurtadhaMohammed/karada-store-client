311ae2bb061ed6c9aa37448e6ba2225cb722f8f1
Baker Alazzawi <baker.alazzawi0@gmail.com>
2025-02-05 11:59:23 +0300

    Update loginForm.jsx

--- src/app/login/Form/loginForm.jsx
+++ src/app/login/Form/loginForm.jsx
@@ -14,6 +14,7 @@ const LoginForm = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const router = useRouter();
 
   const searchParams = useSearchParams();
@@ -29,7 +30,25 @@ const LoginForm = () => {
   } = useAppStore();
   const handleChange = (otp) => setOtp(otp);
   const globalPhone = userInfo?.phone;
+
+  const handlePhoneChange = (e) => {
+    const value = e.target.value;
+    setPhone(value);
+    if (value !== "07700000000") {
+      const isValid = validateIraqiPhoneNumber(value);
+      if (!isValid && value.length > 0) {
+        setError("يرجى إدخال رقم هاتف صالح");
+      } else {
+        setError("");
+      }
+    } else {
+      setError("");
+    }
+  };
+
   const handleLogin = async () => {
+    if (phone !== "07700000000" && validateIraqiPhoneNumber(phone) === false)
+      return setError("يرجى إدخال رقم هاتف صالح");
     setLoading(true);
     const resp = await apiCall({
       pathname: `/client/auth/login`,
@@ -44,6 +63,8 @@ const LoginForm = () => {
     if (resp?.message == "Login Success") {
       setIsOtp(true);
       router.replace(`/login?phone=${phone}`);
+    } else {
+      setError("يرجى إدخال رقم هاتف صالح");
     }
   };
 
@@ -62,11 +83,15 @@ const LoginForm = () => {
     if (resp.accessToken) {
       localStorage.setItem("karada-token", resp.accessToken);
       localStorage.setItem("karada-refreshToken", resp.refreshToken);
+      localStorage.setItem("karada-user", JSON.stringify(resp.user));
       updateUserInfo(resp.user);
       router.replace("/");
       setIsLogin(true);
+    } else {
+      setError("يرجى إدخال رمز التحقق صحيح");
     }
   };
+
   useEffect(() => {
     const phoneFromParams = searchParams.get("phone");
     if (phoneFromParams) {
@@ -138,6 +163,7 @@ const LoginForm = () => {
             </div>
           </Container>
         </div>
+        <div>{error && <p className="text-red-500">{error}</p>}</div>
       </div>
     );
 
@@ -156,7 +182,7 @@ const LoginForm = () => {
           <div className="h-[16px]"></div>
           <Input
             value={phone}
-            onChange={(e) => setPhone(e.target.value)}
+            onChange={handlePhoneChange}
             hint="رقم الهاتف"
           />
         </div>
@@ -186,6 +212,7 @@ const LoginForm = () => {
           </div>
         </Container>
       </div>
+      <div>{error && <p className="text-red-500">{error}</p>}</div>
     </div>
   );
 };
