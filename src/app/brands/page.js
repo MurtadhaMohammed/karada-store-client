fb11afdef7f5fce09e3c91e048b26bbba9c43e25
Kadhim Salah Kadhim <ka02zm45@gmail.com>
2025-02-06 10:51:10 +0300

    error boundry

--- src/app/brands/page.js
+++ src/app/brands/page.js
@@ -2,23 +2,30 @@ import BrandList from "./BrandsList/brandList";
 import { URL } from "@/lib/api";
 import BrandSkeleton from "./Skeleton/brandSkeleton";
 import SearchBar from "@/components/SearchBar/searchBar";
+import ErrorBoundary from "@/components/ErrorBoundry/errorBoundry";
 
 export default async function Brands() {
-  const res = await fetch(`${URL}/client/brand/all-brands?page=1&limit=100`, {
-    method: "GET",
-    cache: "no-cache",
-  });  
-
-  if (!res.ok) throw new Error("Failed to fetch data");
-
-  const data = await res.json();
-
-  return (
-    <div className="pb-[100px]">
-      <SearchBar/>
-      <BrandList brands={data.records} />
-
-      {/* <BrandSkeleton /> */}
-    </div>
-  );
+  try{
+    const res = await fetch(`${URL}/client/brand/all-brands?page=1&limit=100`, {
+      method: "GET",
+      cache: "no-cache",
+    });  
+  
+    if (!res.ok) throw new Error("Failed to fetch data");
+  
+    const data = await res.json();
+  
+    return (
+      <div className="pb-[100px]">
+        <SearchBar/>
+        <BrandList brands={data.records} />
+  
+        {/* <BrandSkeleton /> */}
+      </div>
+    );
+  }
+  catch (error) {
+    console.error("Error fetching view data:", error);
+    return <div><ErrorBoundary/></div>;
+  }
 }
