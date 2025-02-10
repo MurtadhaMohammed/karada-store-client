2e119574df3cce7290ed7cbd3b8797ce75654005
MurtadhaMohammed <mumu9943@gmail.com>
2025-02-10 15:09:34 +0300

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,8 +3,8 @@ import LoginFormWeb from "./FormWeb/loginFormWeb";
 
 export default function Login() {
   return (
-    <div className="pb-[100px] h-[100vh]">
-      <div className="md:hidden block">
+    <div className="pb-[100px] h-[100vh] overflow-hidden">
+      <div className="md:hidden block h-full overflow-hidden">
         <LoginForm />
       </div>
       <div className="md:block hidden">
