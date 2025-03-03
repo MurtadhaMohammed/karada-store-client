d300c03ed5e5c63b4c58d8994f9558963ab60e8d
MurtadhaMohammed <mumu9943@gmail.com>
2025-03-03 15:59:52 +0300

    update

--- src/components/DefaultCard/defaultCard.jsx
+++ src/components/DefaultCard/defaultCard.jsx
@@ -107,8 +107,8 @@ const DefaultCard = ({
             <div>
               {item?.price !== item?.endPrice && (
                 <h4 className="text-[14px] font-normal text-[#a5a5a5] line-through">
-                  <span className="text-[12px]">د.ع</span>
                   {Number(item?.price).toLocaleString("en")}
+                  <span className="text-[12px]"> د.ع </span>
                 </h4>
               )}
 
