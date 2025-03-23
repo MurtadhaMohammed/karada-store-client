a005b38c9b79cfec649d71507dc9f60f1ca751e3
MurtadhaMohammed <mumu9943@gmail.com>
2025-03-23 10:44:18 +0300

    update

--- src/components/SliderBanner/sliderBanner.jsx
+++ src/components/SliderBanner/sliderBanner.jsx
@@ -63,7 +63,7 @@ export default function SliderBanner({ banners, title }) {
               >
                 <LinkOrDev
                   banner={el}
-                  classNameProp="w-[100%] md:aspect-[3.5] aspect-3 relative rounded-[16px] overflow-hidden pb-[20px] inline-block shadow-md active:opacity-50 transition-all pure-skeleton"
+                  classNameProp="w-[100%] md:aspect-[3.5] aspect-[2.5] relative rounded-[16px] overflow-hidden pb-[20px] inline-block shadow-md active:opacity-50 transition-all pure-skeleton"
                 >
                   <Image
                     src={`${IMAGE_URL}/${el?.img}`}
