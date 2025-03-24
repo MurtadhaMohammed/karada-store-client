da9d06c20b942d9a57b901798993327193495204
GTYaseen <YaseenTahaAlEnezi@gmail.com>
2025-03-24 11:12:31 +0300

    new discount check

--- src/app/checkout/utils/orderUtils.js
+++ src/app/checkout/utils/orderUtils.js
@@ -31,9 +31,12 @@ export const createOrder = async (
         note,
       },
     });
-    if (response?.status == 200) {
+
+    console.log("response", response);
+
+    if (response?.order) {
       clearCart();
-      if (response?.status == "Not Logged In" && !isLogin) {
+      if (response?.status === "Not Logged In" && !isLogin) {
         setIsOtp(true);
         setOtp("");
         router.replace("/login?phone=" + order.phone);
