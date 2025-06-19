313823dba71ff8cc1b70d841181b85445e7bf486
MurtadhaMohammed <mumu9943@gmail.com>
2025-06-19 13:16:23 +0300

    update

--- src/app/checkout/Payments/payments.jsx
+++ src/app/checkout/Payments/payments.jsx
@@ -39,7 +39,7 @@ const Payments = () => {
     {
       value: "installment",
       label: "شراء بالتقسيط",
-      disabled: false,
+      disabled: true,
       onClick: handleInstallmentClick,
     },
   ];
