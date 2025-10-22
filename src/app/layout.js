c67bdab1e3a85f535a39619cf3764537dd5b5acf
Kadhim Salah Kadhim <ka02zm45@gmail.com>
2025-10-22 11:25:57 +0300

    Add Meta Pixel tracking code to layout
    Integrated Facebook Meta Pixel tracking by adding the required script tags and noscript fallback to the layout. This enables page view tracking for analytics and marketing purposes.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import ProgressBar from "./Progreess";
 import MainHeaderWeb from "@/components/MainHeader/web/mainHeaderWeb";
 import Footer from "@/components/Footer/footer";
 import Head from "next/head";
+import Script from "next/script";
 
 const IBMFont = localFont({
   src: [
@@ -141,18 +142,59 @@ export default function RootLayout({ children }) {
           content="width=device-width, initial-scale=1.0, user-scalable=no"
         />
         <meta name="apple-itunes-app" content="app-id=6741197248" />
+        {/* Meta Pixel Code */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1552395819444834&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </Head>
-      <ReactQueryProvider
-        fontStyle={`${newSansFont.variable} ${rubikFont.variable} ${IBMFont.variable}  antialiased`}
-      >
-        <ProgressBar />
-        <MainHeader />
-        <MainHeaderWeb />
-        {children}
-        <BottomNabar />
-        <Footer />
-        <SideMenu />
-      </ReactQueryProvider>
+      <body>
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+            `,
+          }}
+        />
+        <Script
+          id="meta-pixel-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              fbq('init', '1552395819444834');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
+        <ReactQueryProvider
+          fontStyle={`${newSansFont.variable} ${rubikFont.variable} ${IBMFont.variable}  antialiased`}
+        >
+          <ProgressBar />
+          <MainHeader />
+          <MainHeaderWeb />
+          {children}
+          <BottomNabar />
+          <Footer />
+          <SideMenu />
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
